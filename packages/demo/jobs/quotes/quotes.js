73c6c89061566af2c75afcc65f6be734c7394d33
nayakam <thillakan@gmail.com>
2016-08-20 20:36:06 +1000

    Update
    Update

--- packages/demo/jobs/quotes/quotes.js
+++ packages/demo/jobs/quotes/quotes.js
@@ -1,7 +1,61 @@
 module.exports = {
-  onRun: function (config, dependencies, jobCallback) {
-    var _ = dependencies.underscore;
-    var quotes = _.first(_.shuffle(config.quotes), config.limit || 10);
-    jobCallback(null, {quotes: quotes, title: config.widgetTitle});
-  }
+    onRun: function(config, dependencies, jobCallback) {
+
+        var logger = dependencies.logger;
+        logger.info('quote.js - config : ' + config);
+        logger.info('quote.js - Object.keys(config) : ' + Object.keys(config));
+        Object.keys(config).forEach(function(key) {
+            logger.info('quote.js - config key:' + key + ', Value:' + config[key]);
+        });
+
+
+        logger.info('quote.js -  key :' + "quotes" + ', config.hasOwnProperty : ' + config.hasOwnProperty("quotes"));
+
+        var options = {
+            url: config.messageAPIUrl,
+            //  path: '/stats',
+            method: 'GET'
+        };
+
+        dependencies.request(options, function(err, response, body) {
+            logger.info('quote.js - response : ' + response);
+            logger.info('quote.js - err : ' + err);
+            logger.info('quote.js - body : ' + body);
+            if (response != null) {
+                logger.info('quote.js - Object.keys(response) : ' + Object.keys(response));
+
+                logger.info('quote.js - response.toJSON : ' + response.toJSON);
+                logger.info('quote.js - response.statusCode : ' + response.statusCode);
+                logger.info('quote.js - response.headers : ' + response.headers);
+                logger.info('quote.js - response.url : ' + response.url);
+                logger.info('quote.js - response.method : ' + response.method);
+                logger.info('quote.js - response.statusCode : ' + response.statusCode);
+                logger.info('quote.js - response.statusMessage : ' + response.statusMessage);
+                logger.info('quote.js - response.request : ' + response.request);
+                logger.info('quote.js - response.req : ' + response.req);
+                logger.info('quote.js - response.body : ' + response.body);
+                // Object.keys(response).forEach(function(key) {
+                //     logger.info('quote.js - response key:' + key + ', Value:' + response[key]);
+                // });
+            }
+
+            // only supported in Firefox
+            //        for each(var value in config) {
+            //            logger.info('quote.js -  value :' + value);
+            //        }
+            var _ = dependencies.underscore;
+            var quotes = _.first(_.shuffle(config.quotes), config.limit || 10);
+            logger.info('quote.js - config.quotes : ' + quotes);
+            logger.info('quote.js -  Object.keys(quotes) : ' +  Object.keys(quotes));
+            if (quotes != null) {
+                Object.keys(quotes).forEach(function(key) {
+                    logger.info('quote.js - quotes key:' + key + ', Value:' + quotes[key]);
+                });
+            }
+            jobCallback(null, {
+                quotes: body,
+                title: config.widgetTitle
+            });
+        });
+    }
 };
--- packages/demo/widgets/quotes/quotes.js
+++ packages/demo/widgets/quotes/quotes.js
@@ -1,24 +1,45 @@
 widget = {
 
-  onData: function (el, data) {
-    if (data.title) {
-      $('h2', el).text(data.title);
+    onData: function(el, data) {
+        if (data.title) {
+            $('h2', el).text(data.title);
+        }
+
+        var $content = $('.content', el);
+        $content.empty();
+
+        console.log("data:" + data);
+        console.log("data.quotes" + data.quotes);
+        console.log("data.quotes.length:" + data.quotes.length);
+        var json = JSON.parse(data.quotes);
+        console.log("json : " + json);
+        console.log("json.length : " + json.length);
+        console.log(" json.quotes : " + json.quotes);
+        console.log(" json.quotes.length : " + json.quotes.length);
+        console.log('quote.js - Object.keys(json) : ' + Object.keys(json));
+        if (json.quotes != null) {
+            console.log(" json.quotes[0].author : " + json.quotes[0].author);
+            console.log(" json.quotes[0].quote : " + json.quotes[0].quote);
+            $content.append(
+                "<blockquote>" + json.quotes[0].quote + "<cite>" + json.quotes[0].author + "</cite></blockquote>"
+            );
+        } else {
+            $content.append(
+                "<blockquote>NO QUOTES FOUND<blockquote>"
+            );
+        }
+
+        // if (data.quotes.length > 0) {
+        //     // data.quotes.forEach(function (quote) {
+        //     //   $content.append(
+        //     //       "<blockquote>" + quote.quote + "<cite>" + quote.author + "</cite></blockquote>"
+        //     //   );
+        //     // });
+        //
+        // } else {
+        //     $content.append(
+        //         "<blockquote>NO QUOTES FOUND<blockquote>"
+        //     );
+        // }
     }
-
-    var $content = $('.content', el);
-    $content.empty();
-
-    if (data.quotes.length > 0) {
-      data.quotes.forEach(function (quote) {
-        $content.append(
-            "<blockquote>" + quote.quote + "<cite>" + quote.author + "</cite></blockquote>"
-        );
-      });
-
-    } else {
-      $content.append(
-          "<blockquote>NO QUOTES FOUND<blockquote>"
-      );
-    }
-  }
 };
